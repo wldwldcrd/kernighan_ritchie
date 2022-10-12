74befc1da1ea4566257a2c26fd9cbcbb58f37fa6
wldwldcrd <wldwldcrd@gmail.com>
2022-10-12 08:18:33 +0300

    5.9 was done

--- chapter_5/5_8_day_of_year.c
+++ chapter_5/5_8_day_of_year.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static char daytab[2][13] = {
-    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+// static char daytab[2][13] = {
+//     {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+//     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+static char daytab_non_leap[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+static char daytab_leap[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+static char *daytab[2] = {daytab_non_leap, daytab_leap};
 
 int day_of_year(int year, int month, int day);
 void month_day(int year, int yearday, int *pmonth, int *pday);
@@ -50,7 +53,7 @@ int day_of_year(int year, int month, int day)
     leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     for (i = 1; i < month; i++)
     {
-        day += daytab[leap][i];
+        day += *(daytab[leap] + i);
     }
     return day;
 }
@@ -62,7 +65,7 @@ void month_day(int year, int yearday, int *pmonth, int *pday)
     leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     for (i = 1; yearday > daytab[leap][i]; i++)
     {
-        yearday -= daytab[leap][i];
+        yearday -= *(daytab[leap] + i);
     }
     *pmonth = i;
     *pday = yearday;
